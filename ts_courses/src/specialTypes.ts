32d64d6db45f02e7ec0f19217063bf38fb343fa6
pm.bovios <pm.bovios@gmail.com>
2025-09-30 00:26:13 +0300

    special types practice

---
+++ ts_courses/src/specialTypes.ts
@@ -0,0 +1,55 @@
+// special types
+// any
+// let value: any;
+// value = 5;
+// value = [];
+// value = {};
+
+// function logData(data: any[]) {
+//   console.log(data);
+// }
+
+// unknown
+// function logData(data: unknown) {
+//   let value: string;
+//   if (typeof data === "string") {
+//     value = data;
+//   }
+
+//   if (Array.isArray(data)) {
+//     data;
+//   }
+// }
+
+// never
+// enum Values {
+//   FIRST,
+//   SECOND,
+//   THIRD, //will be error
+// }
+
+// function fn(value: Values) {
+//   switch (value) {
+//     case Values.FIRST:
+//       return 1;
+//     case Values.SECOND:
+//       return 2;
+//     default:
+//       const exhaustiveCheck: never = value;
+//       return value;
+//   }
+// }
+
+// fn(Values.FIRST);
+// fn(Values.SECOND);
+
+// type RandomName = string & number;
+
+// const value: RandomName = never; //error
+
+// void
+// function fn(): void {
+//   console.log();
+// }
+
+// type Fn = (arg: number, arg2: string) => void;
